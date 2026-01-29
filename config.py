4aff8e86480bc25e41bc5becd3e9bb73005f844f
bobr <bobr@email>
2026-01-29 22:34:06 +0300

    chore: config setted up

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-TELEGRAM_BOT_TOKEN = None
-DATABASE_PATH = None
-BINANCE_API_URL = None
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+DATABASE_PATH = os.getenv("DATABASE_PATH", "bot.db")
+BINANCE_API_URL = os.getenv("BINANCE_API_URL", "https://api.binance.com")
